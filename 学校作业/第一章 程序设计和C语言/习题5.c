5018cb78a3550a5d5149ece0a8f0104ea56bcc08
ADDD0 <448417505@qq.com>
2018-03-12 22:03:01 +0800

    22:02 2018/3/12

--- 学校作业/第一章 程序设计和C语言/习题5.c
+++ 学校作业/第一章 程序设计和C语言/习题5.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+main()
 {
 	printf("*****\n  *****\n    *****\n      *****\n");
-	return 0;
 }
--- 学校作业/第一章 程序设计和C语言/习题6.c
+++ 学校作业/第一章 程序设计和C语言/习题6.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+main()
 {
 	int a, b, c;
 	scanf("%d, %d, %d", &a, &b, &c);
-	if(a > b)b = a;
-	if(b > c)c = b;
+	if(a > b)
+		b = a;
+	if(b > c)
+		c = b;
 	printf("max=%d\n", c);
-	return 0;
 }
