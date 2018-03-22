68dbe52b24edb4982b40cbd4ada68b4eabeeaf7b
ADDD0 <448417505@qq.com>
2018-03-22 22:52:03 +0800

    22:51 2018/3/22

---
+++ The C Programming Language/第一章 导言/1.10 外部变量与作用域/练习1-21.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define  TABINC  8					/*	制表符增量			*/
+
+/*	编写程序entab，将空格串替换为最少数量
+的制表符和空格，但要保持单词之间的间隔不变					*/
+main()
+{
+	int c, nb, nt, pos;
+	
+	nb = 0;							/*	必要的空格数		*/
+	nt = 0;							/*	必要的制表符数目	*/
+	for (pos = 1; (c = getchar()) != EOF; ++pos)
+		if (c == ' ') {
+			if (pos % TABINC != 0)
+				++nb;				/*	增加空格数目		*/
+			else {
+				nb = 0;				/*	重置空格数目		*/
+				++nt;				/*	加一个制表符		*/
+			}
+		} else {
+				for ( ; nt > 0; --nt)
+					putchar('\t');	/*	输出制表符			*/
+				if (c == '\t')		/*	忽略空格			*/
+					nb = 0;
+				else				/*	输出空格			*/
+					for ( ; nb > 0; --nb)
+						putchar(' ');
+				putchar(c);
+				if (c == '\n')
+					pos = 0;
+				else if (c == '\t')
+					pos = pos + (TABINC - (pos-1) % TABINC) - 1;
+		}
+}
--- The C Programming Language/第一章 导言/1.10 外部变量与作用域/练习1-22.c
+++ The C Programming Language/第一章 导言/1.10 外部变量与作用域/练习1-22.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/*	编写一个程序，把较长的输入行“折”成短一些的两行或
+多行，折行的位置在输入行的第n列之前的最后一个非空格之后*/
+main()
+{
+	int c;
+	
+	while ((c = getchar()) != EOF) {
+		if (c == ' ') {
+			putchar('\n');
+			while ((c = getchar()) == ' ') {
+				;
+			}
+		}
+		putchar(c);
+	}
+}
