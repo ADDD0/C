f9ec386fcfcd747571118038a1235e81548485d0
ADDD0 <448417505@qq.com>
2018-04-18 22:54:15 +0800

    22:54 2018/4/18

---
+++ 欧拉计划(Project Euler)/001.c
@@ -0,0 +1,13 @@
+/* 找出1000以下自然数中3和5的倍数之和 */
+main()
+{
+    int i, sum = 0;
+
+    for (i = 3; i < 1000; i+=3)
+        sum += i;
+    for (i = 5; i < 1000; i+=5)
+        sum += i;
+    for (i = 15; i < 1000; i += 15)
+        sum -= i;
+    printf("%d\n", sum);
+}
