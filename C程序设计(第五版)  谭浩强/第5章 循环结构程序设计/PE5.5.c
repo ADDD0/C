f0c539bbd69cfec3e31955af68f70917fffb5014
ADDD0 <448417505@qq.com>
2018-05-28 22:02:48 +0800

    22:02 2018/5/28

--- C程序设计(第五版)  谭浩强/第5章 循环结构程序设计/PE5.5.c
+++ C程序设计(第五版)  谭浩强/第5章 循环结构程序设计/PE5.5.c
@@ -1,14 +1,16 @@
 /* 求S=a+aa+aaa+…+aa…a(n个a)之值,其中a是一个数字,n表示a的位数,n由键盘输入 */
 #include <stdio.h>
 
-#define  A  2
-
 main()
 {
-    int a, n, i, sum;
+    int a, n, tsum, sum;
 
-    scanf("%d", &n);
-    for (a = A, i = sum = 0; n > 0; --n, sum += i)
-        i = i * 10 + a;
+    printf("Input a and n:");
+    scanf("%d %d", &a, &n);
+    for (tsum = sum = 0; n; --n) {
+        tsum += a;
+        a *= 10;
+        sum += tsum;
+    }
     printf("%d\n", sum);
 }
